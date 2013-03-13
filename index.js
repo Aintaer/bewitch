2abfe2f4cec0c0faecf00b106cfa4023a2e59ecb
Alex Lee <aintaer@aintaer.com>
2013-03-13 13:56:32 -0400

    remote copy through rsync proof of concept

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 
+/*jslint stupid:true */
+/*global process */
 (function() {
   'use strict';
 
-  var Sweep = require('./lib/sweep'),
-  watcher;
+  var Watcher = require('./lib/watcher'),
+  fs = require('fs'),
+  exec = require('child_process').exec,
+  watcher, src, dest;
 
-  watcher = new Sweep('test', console.log);
+  src = 'test';
+  src = fs.realpathSync(src /*, cache */);
+  dest = 'aintaer@solid.be.lan:/home/aintaer/test';
+
+  watcher = new Watcher(src, function(o) {
+    exec('rsync -rltz -e ssh --executability . '+dest, 
+         {cwd: src}, 
+         function(err, stdout, stderr) {
+    });
+    console.log(src, Object.keys(o), '->', dest);
+  });
 }());
