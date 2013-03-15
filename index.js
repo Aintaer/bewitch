6ad446f7335a3660872efb31e750e69078b66551
Alex Lee <aintaer@aintaer.com>
2013-03-15 18:28:37 -0400

    Completed the remote copy path through SSH
    Multiplexes with ssh_master module, it creates a master connection which
    must be destroyed by calling master.destroy();
    
    rsync module accepts a master socket to copy through.

--- index.js
+++ index.js
@@ -5,20 +5,60 @@
 (function() {
   'use strict';
 
-  var Watcher = require('./lib/watcher'),
+  var routes = require('./config.json'),
+  Watcher = require('./lib/watcher'),
+  Master = require('./lib/ssh_master'),
+  rsync = require('./lib/rsync'),
   fs = require('fs'),
-  exec = require('child_process').exec,
-  watcher, src, dest;
+  masters = [],
+  watchers = [];
 
-  src = 'test';
-  src = fs.realpathSync(src /*, cache */);
-  dest = 'aintaer@solid.be.lan:/home/aintaer/test';
+  function spawn(routes) {
+    var sources = Object.keys(routes);
 
-  watcher = new Watcher(src, function(o) {
-    exec('rsync -rltz -e ssh --executability . '+dest, 
-         {cwd: src}, 
-         function(err, stdout, stderr) {
-    });
-    console.log(src, Object.keys(o), '->', dest);
+    (function spawnMaster() {
+      if (!sources.length) { return; }
+      var src = sources.shift(),
+      dest = routes[src],
+      master;
+
+      src = fs.realpathSync(src);
+
+      master = new Master( dest.split(':')[0] );
+      master.on('connection', function(socket) {
+        console.log('Listening to '+src);
+        var watcher = new Watcher(src, function(o) {
+          console.log.apply(console, Object.keys(o));
+          rsync.copy(src, dest, function() {
+            console.log(src, '->', dest);
+          }, socket);
+        });
+        watchers.push(watcher);
+
+        spawnMaster();
+      });
+      masters.push(master);
+    }());
+  }
+
+  // Spawn all the routes!
+  spawn( routes );
+
+  process.on('SIGINT', function() {
+    process.exit();
   });
+
+  process.on('exit', function destroy() {
+    if (masters.length) {
+      masters.forEach(function(master) {
+        master.destroy();
+      });
+    }
+    if (watchers.length) {
+      watchers.forEach(function(watcher) {
+        watcher.destroy();
+      });
+    }
+  });
+
 }());
